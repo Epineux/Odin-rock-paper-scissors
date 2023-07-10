0a571c0ebc75b117a7dbb64aa479b778108d0ea7
Epineux <martinlecontehelloworld@gmail.com.noreply.github.com>
2023-07-11 01:04:32 +0900

    Make it 5 rounds display the score and the winner

--- script.js
+++ script.js
@@ -1,93 +1,103 @@
 const availableMoves = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
-function playRound() {
+function playRound(e) {
 
-    let playerChoice = prompt("What do you play ? (rock | paper | scissors)");
+    // Function to determine who won considering player and computer choice
+    function checkWinner(player, computer) {
 
-    // Is the user input valid ? (case insensitive)
-    if (playerChoice.localeCompare(availableMoves[0], undefined, { sensitivity: 'accent' }) !== 0 &&
-    playerChoice.localeCompare(availableMoves[1], undefined, { sensitivity: 'accent' }) !== 0 &&
-    playerChoice.localeCompare(availableMoves[2], undefined, { sensitivity: 'accent' }) !== 0) {
-        alert("This is not a valid choice !");
-    } else {
-        // Function to determine who won considering player and computer choice
-        function checkWinner(player, computer) {
+        switch(player.toUpperCase()) {
 
-            switch(player.toUpperCase()) {
+            case 'ROCK':
+                switch(computer) {
+                    case 'rock':
+                        showWinner("draw");
+                        break;
+                    case 'paper':
+                        showWinner("computer");
+                        break;
+                    default:
+                        showWinner("player");
+                }
+                break;
 
-                case 'ROCK':
-                    switch(computer) {
-                        case 'rock':
-                            showWinner("draw");
-                            break;
-                        case 'paper':
-                            showWinner("computer");
-                            break;
-                        default:
-                            showWinner("player");
-                    }
-                    break;
+            case 'PAPER':
+                switch(computer) {
+                    case 'rock':
+                        showWinner("player");
+                        break;
+                    case 'paper':
+                        showWinner("draw");
+                        break;
+                    default:
+                        showWinner("computer");
+                }
+                break;
 
-                case 'PAPER':
-                    switch(computer) {
-                        case 'rock':
-                            showWinner("player");
-                            break;
-                        case 'paper':
-                            showWinner("draw");
-                            break;
-                        default:
-                            showWinner("computer");
-                    }
-                    break;
-
-                default:
-                    switch(computer) {
-                        case 'rock':
-                            showWinner("computer");
-                            break;
-                        case 'paper':
-                            showWinner("player");
-                            break;
-                        default:
-                            showWinner("draw");
-                    }
-            }
+            default:
+                switch(computer) {
+                    case 'rock':
+                        showWinner("computer");
+                        break;
+                    case 'paper':
+                        showWinner("player");
+                        break;
+                    default:
+                        showWinner("draw");
+                }
         }
-        // To show the winner
-        function showWinner(winner) {
+    }
+    // To show the winner
+    function showWinner(winner) {
 
-            switch(winner) {
-                
-                case("player"):
-                    console.log("The player won");
-                    playerScore++;
-                    break;
-                case("computer"):
-                    console.log("The computer won");
-                    computerScore++;
-                    break;
-                default:
-                    console.log("It's a draw");
-            }
+        switch(winner) {
+            
+            case("player"):
+                document.querySelector(".winner").textContent = "The player won";
+                playerScore++;
+                showScore();
+                break;
+            case("computer"):
+                document.querySelector(".winner").textContent = "The computer won";
+                computerScore++;
+                showScore();
+                break;
+            default:
+                document.querySelector(".winner").textContent = "It's a draw";
         }
-        // The computer choose rock paper or scissors
-        let chooseMove = ()=> availableMoves[Math.floor(Math.random() * availableMoves.length)];
-        computerChoice = chooseMove();
-        
-        console.log(`The player choose ${playerChoice.toUpperCase()}`);
-        console.log(`The computer choose ${computerChoice.toUpperCase()}`);
-        checkWinner(playerChoice, computerChoice);
+    }
+
+    // To display the score
+    function showScore() {
+        document.querySelector('.player').textContent = `Player score : ${playerScore}`;
+        document.querySelector('.computer').textContent = `Computer score : ${computerScore}`;
+    }
+
+    let playerChoice = e.target.dataset.move;
+    console.log(playerChoice);
+    // The computer choose rock paper or scissors
+    let chooseMove = ()=> availableMoves[Math.floor(Math.random() * availableMoves.length)];
+    computerChoice = chooseMove();
+    
+    console.log(`The player choose ${playerChoice.toUpperCase()}`);
+    console.log(`The computer choose ${computerChoice.toUpperCase()}`);
+    checkWinner(playerChoice, computerChoice);
+    if (playerScore === 5 || computerScore === 5) {
+        buttons.forEach(button => button.removeEventListener('click', playRound));
+        switch(playerScore) {
+            case 5:
+                document.querySelector('.score').innerHTML = 'The player has won !';
+                break;
+            default:
+                document.querySelector('.score').innerHTML = 'The computer has won !';
+        }
     }
 }
 
-// A game is 5 round
-function playGame() {
-    for(let i = 0; i < 5; i++) {
-        console.log(`The score is ${playerScore} for the player and ${computerScore} for the computer.`)
-        playRound();
-    }
-}
 
-playGame();
+    
+const buttons = document.querySelectorAll('button');
+console.log(buttons);
+buttons.forEach(button => button.addEventListener('click', playRound));
+
+
